9b8fbc40632fff50beb72527431c995d854c3ef8
Mike Kanning <mikekanning@gmail.com>
2018-12-22 00:33:04 -0500

    add spec to debug expiry portion of sync. addresses GAL-16

--- app/models/title.rb
+++ app/models/title.rb
@@ -11,21 +11,13 @@ class Title < ApplicationRecord
   end
 
   def self.sync(titles)
-    outcome = {
-      new: 0, updated: 0, expired: expire_titles(titles.first.institution)
-    }
+    outcome = { new: 0, expired: expire_titles(titles.first.institution) }
     titles.each do |title|
       existing_title = Title.where(mms_id: title.mms_id)
-      if existing_title.any?
-        # TODO: Sometimes MMS IDs are duplicated in a single report- figure that out before this can be enabled
-        # in the meantime ignore if MMS ID exists, but display count
-        puts "Duplicate MMS ID found in report: #{title.mms_id}"
-        # updated_title = existing_title.update(title.attributes(except: :id))
-        outcome[:updated] += 1
-      else
-        saved_title = title.save
-        outcome[:new] += 1 if saved_title
-      end
+      next if existing_title.any?
+
+      saved_title = title.save
+      outcome[:new] += 1 if saved_title
     end
     outcome
   end
--- spec/models/titles_spec.rb
+++ spec/models/titles_spec.rb
@@ -3,30 +3,38 @@
 require 'rails_helper'
 
 describe Title do
-  # context 'basic attributes' do
-  #   it 'has a name' do
-  #     expect(Institution.new).to respond_to :name
-  #   end
-  #   it 'has a institution code' do
-  #     expect(Institution.new).to respond_to :institution_code
-  #   end
-  #   it 'has a image' do
-  #     expect(Institution.new).to respond_to :image
-  #   end
-  #   it 'has an api key' do
-  #     expect(Institution.new).to respond_to :api_key
-  #   end
-  # end
-  # context 'api auth functionality' do
-  #   let(:institution) { Institution.new(institution_code: 'GALI') }
-  #   it 'has an api_key' do
-  #     expect(institution.api_key).to eq 'GALI_key'
-  #   end
-  #   context 'in production' do
-  #     it 'has a production API key' do
-  #       allow(Rails).to receive(:env) { 'production'.inquiry }
-  #       expect(institution.api_key.length).to eq 32
-  #     end
-  #   end
-  # end
+  context 'basic attributes' do
+    it 'has a name' do
+      expect(Institution.new).to respond_to :name
+    end
+    it 'has a institution code' do
+      expect(Institution.new).to respond_to :institution_code
+    end
+    it 'has a image' do
+      expect(Institution.new).to respond_to :image
+    end
+    it 'has an api key' do
+      expect(Institution.new).to respond_to :api_key
+    end
+  end
+  context 'api auth functionality' do
+    let(:institution) { Institution.new(institution_code: 'GALI') }
+    it 'has an api_key' do
+      expect(institution.api_key).to eq 'GALI_key'
+    end
+    context 'in production' do
+      it 'has a production API key' do
+        allow(Rails).to receive(:env) { 'production'.inquiry }
+        expect(institution.api_key.length).to eq 32
+      end
+    end
+  end
+  context 'sync functionality' do
+    it 'expires a title more than 90 days old' do
+      Fabricate(:title, created_at: Time.now - 100.days)
+      Title.expire_titles(Title.last.institution)
+      expect(Title.count).to eq 0
+    end
+  end
+
 end
