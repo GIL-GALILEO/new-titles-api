c01dc4b011d3e256d0f559e3381d3f804929de18
Seamus Narron <seamus@uga.edu>
2019-05-22 12:58:18 -0400

    Updated gatech's shortcode, institution_code, api_key, and url in the institution database

--- db/migrate/20190521161541_update_georgia_tech.rb
+++ db/migrate/20190521161541_update_georgia_tech.rb
@@ -1,7 +1,10 @@
 class UpdateGeorgiaTech < ActiveRecord::Migration[5.2]
   def up
     gt = Institution.find_by_shortcode('tech')
-    gt.name = 'Georgia Institute of Technology Library'
+    gt.shortcode= 'gatech'
+    gt.institution_code = '01GALI_GIT'
+    gt.api_key = '01GALI_GIT_key'
+    gt.url = 'https://gatech-primo.hosted.exlibrisgroup.com/primo-explore/search?vid=01GALI_GIT&search_scope=default_scope&query=addsrcrid,exact,'
     gt.electronic_path =
         '/shared/Georgia Institute of Technology Library/Reports/USG/USG New Titles Electronic'
     gt.physical_path =
@@ -10,8 +13,11 @@ class UpdateGeorgiaTech < ActiveRecord::Migration[5.2]
   end
 
   def down
-    gt = Institution.find_by_shortcode('tech')
-    gt.name = 'Georgia Institute of Technology'
+    gt = Institution.find_by_shortcode('gatech')
+    gt.shortcode = 'tech'
+    gt.institution_code = ''
+    gt.api_key = '_key'
+    gt.url = 'https://www.library.gatech.edu/'
     gt.electronic_path = nil
     gt.physical_path = nil
     gt.save!
